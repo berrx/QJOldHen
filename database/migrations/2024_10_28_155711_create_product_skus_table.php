3496c335b0804f6514fcf7cbf67b29ed209a7e51
berrx <berrx@outlook.com>
2024-10-29 00:19:45 +0800

    修改迁徙文件

--- database/migrations/2024_10_28_155711_create_product_skus_table.php
+++ database/migrations/2024_10_28_155711_create_product_skus_table.php
@@ -14,16 +14,14 @@ class CreateProductSkusTable extends Migration
     public function up()
     {
         Schema::create('product_skus', function (Blueprint $table) {
-            Schema::create('product_skus', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('title');
-                $table->string('description');
-                $table->decimal('price', 10, 2);
-                $table->unsignedInteger('stock');
-                $table->unsignedBigInteger('product_id');
-                $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-                $table->timestamps();
-            });
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('stock');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
