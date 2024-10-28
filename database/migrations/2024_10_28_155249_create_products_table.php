10adfaba04a951f607f9cd53abf3d7ced604cf54
berrx <berrx@outlook.com>
2024-10-29 00:01:34 +0800

    商品数据库创建

--- database/migrations/2024_10_28_155249_create_products_table.php
+++ database/migrations/2024_10_28_155249_create_products_table.php
@@ -14,7 +14,15 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->boolean('on_sale')->default(true);
+            $table->float('rating')->default(5);
+            $table->unsignedInteger('sold_count')->default(0);
+            $table->unsignedInteger('review_count')->default(0);
+            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }
--- database/migrations/2024_10_28_155711_create_product_skus_table.php
+++ database/migrations/2024_10_28_155711_create_product_skus_table.php
@@ -14,8 +14,16 @@ class CreateProductSkusTable extends Migration
     public function up()
     {
         Schema::create('product_skus', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            Schema::create('product_skus', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('title');
+                $table->string('description');
+                $table->decimal('price', 10, 2);
+                $table->unsignedInteger('stock');
+                $table->unsignedBigInteger('product_id');
+                $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+                $table->timestamps();
+            });
         });
     }
 
