3f4dd156f37d25ffbdc04ca76c1fd7ece8e3a31c
berrx <berrx@outlook.com>
2024-11-02 11:17:26 +0800

    sku

--- database/migrations/2024_10_12_000000_create_product_skus_table.php
+++ database/migrations/2024_10_12_000000_create_product_skus_table.php
@@ -14,12 +14,12 @@ class CreateProductSkusTable extends Migration
     public function up()
     {
         Schema::create('product_skus', function (Blueprint $table) {
-            $table->id;
+            $table->bigIncrements('id');
             $table->string('title');
             $table->string('description');
             $table->decimal('price', 10, 2);
             $table->unsignedInteger('stock');
-            $table->unsignedBigInteger('product_id');
+            $table->unsignedInteger('product_id');
             $table->foreign('product_id')->references('id')->on('product')->onDelete('cascade');
             $table->timestamps();
         });
